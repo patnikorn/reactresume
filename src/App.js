d57acf9691e528e70449d110aebfd5787dcc59ef
patnikorn <patnikorn.ta.61@ubu.ac.th>
2021-02-24 05:12:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './watermelon.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>PATNIKORN DEV.</b>
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h6>Getting Started</h6>
+        </a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
